219427074d33204db145b15f6c742a91b6f2bbe4
Teddy Kalp <teddy.kalp@sciex.com>
2025-01-07 14:53:51 -0700

    fix storage class name issue

--- devops/cvmfs-full-setup-basic-galaxy/galaxy-services.sh
+++ devops/cvmfs-full-setup-basic-galaxy/galaxy-services.sh
@@ -3,22 +3,23 @@ function install_cvmfs(){
     git clone https://github.com/lablytics/cvmfs-csi /home/ubuntu/cvmfs-csi
     microk8s helm3 install cvmfs /home/ubuntu/cvmfs-csi/deployments/helm/cvmfs-csi -n kube-system -f /home/ubuntu/cvmfs-values.yaml
     echo "CVMFS installed successfully"
-    sleep 120
+}
+
+# Install Galaxy dependencies -> This is new and does not work out of the box as the authors of Galaxy expected. 
+# There needs to better documentation on how to install Galaxy on Kubernetes with the new version that is deployed.
+function install_galaxy_deps(){
+    # git clone https://github.com/galaxyproject/galaxy-helm-deps.git /home/ubuntu/galaxy-helm-deps
+    # cd /home/ubuntu/galaxy-helm-deps/galaxy-deps
+    # sudo helm dependency build
+    # sudo microk8s helm3 install -n kube-system galaxy-project /home/ubuntu/galaxy-helm-deps/galaxy-deps --set cvmfs.storageClassName=cvmfs #--set cvmfs.deploy=false
 }
 
 function install_galaxy(){
     echo "Installing Galaxy on K8s..."
-    microk8s kubectl create namespace galaxy
     microk8s kubectl apply -f /home/ubuntu/galaxy-pvc.yaml 
-    echo "Sleeping for 15 seconds..."
     sleep 15
-    echo "Waking up..."
-    # Install Galaxy dependencies -> This is new and does not work out of the box as the authors of Galaxy expected. There needs to better documentation on how to install Galaxy on Kubernetes.
-    # git clone https://github.com/galaxyproject/galaxy-helm-deps.git /home/ubuntu/galaxy-helm-deps
-    # cd /home/ubuntu/galaxy-helm-deps/galaxy-deps
-    # sudo helm dependency build
-    # helm install -n kube-system galaxy-project /home/ubuntu/galaxy-helm-deps/galaxy-deps --set cvmfs.storageClassName=cvmfs #--set cvmfs.deploy=false
-
+    echo "PVC created successfully"
+    
     # Install Galaxy
     git clone https://github.com/galaxyproject/galaxy-helm.git /home/ubuntu/galaxy-helm
     cd /home/ubuntu/galaxy-helm/galaxy
@@ -30,13 +31,8 @@ function install_galaxy(){
 
 function main(){
     install_cvmfs
-    # sleep for 30 seconds
-    echo "Sleeping for 30 seconds..."
     sleep 30
-    echo "Waking up..."
     install_galaxy
 }
 
 main
-
-# microk8s helm3 upgrade --install galaxy-project /home/ubuntu/galaxy-helm/galaxy -f /home/ubuntu/galaxy-values.yaml -n galaxy
